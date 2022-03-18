c942a8e0ccbd0ae0dd5029a1fc439578d1a951c3
IsraCardoso <israelalmeidacardoso@gmail.com>
2022-03-18 10:29:35 -0300

    Commit07

---
+++ front/front/src/Department.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export class Department extends Component {
+  render() {
+    return (
+      <div>
+        <h3>Departamentos</h3>
+      </div>
+    );
+  }
+}
---
+++ front/front/src/Employee.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export class Employee extends Component {
+  render() {
+    return (
+      <div>
+        <h3>Funcionários</h3>
+      </div>
+    );
+  }
+}
